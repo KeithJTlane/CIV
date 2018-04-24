09d6e74aee3e90003046c162316c914e675b0777
KeithJTlane <37744377+KeithJTlane@users.noreply.github.com>
2018-04-24 20:27:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,15 @@
  <h2>Finally working I think???</h2>
 <h1 style="color:blue;">This is a very Blue Heading</h1>
 <?php
-
- 
- 
 ?>
-
+<section id="content">
+		<p>You can reach me at <a href="mailto:john@smith.com?Subject=CV">john@smith.com</a>....</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta feugiat ante, in fringilla 
+		ex dictum dictum. Nulla sed arcu nisl. Nunc congue, libero in hendrerit fringilla, massa nisi maximus odio, 
+		sit amet pretium nisl eros a justo. Nam eget arcu sed felis consectetur tempus eu eget justo. In laoreet massa vitae fermentum semper. 
+		Suspendisse convallis, purus vitae faucibus placerat, sapien metus consectetur felis, sed consectetur odio sem sit amet orci. Integer 
+		bibendum maximus odio vel vestibulum. Suspendisse potenti. Aliquam porta ullamcorper risus nec tincidunt.
+		</p>
+	</section>
 </body>
 </html>
