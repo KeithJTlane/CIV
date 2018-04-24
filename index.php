71955664fb39c835d7076b78d1e73cce956ce472
KeithJTlane <37744377+KeithJTlane@users.noreply.github.com>
2018-04-24 19:41:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
-
+ <title> this is webserver</title>
 <body>
 
-<h1>My first PHP</h1>
+<h1>My first PHP </h1>
 
 <?php
 echo "Hello World ";
+ echo "please work ";
  
  
 ?>
